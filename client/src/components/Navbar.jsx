8627677416d175ad10750da9d49235699833f160
leliso <lelisoagegnehu491@gmail.com>
2023-05-03 18:01:08 +0300

    add left side navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
+import FlexBetween from "./FlexBetween";
+import { Menu, Search } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+
+function Navbar() {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  return (
+    <AppBar sx={{ position: "static", background: "none", boxShadow: "none" }}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* LEFT SIDE */}
+        <FlexBetween>
+          <IconButton onClick={() => console.log("open/close sidebar")}>
+            <Menu />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,5 +1,15 @@
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
 
 export default Layout;
