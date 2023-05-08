599acce235f58553ec3c40a55fa7e07132cf04bb
leliso <lelisoagegnehu491@gmail.com>
2023-05-08 21:15:34 +0300

    update

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -62,7 +62,7 @@ export const getTransactions = async (req, res) => {
       .limit(pageSize);
 
     const total = await Transaction.countDocuments({
-      name: { $regex: search, $options: "i" },
+      cost: { $regex: search, $options: "i" },
     });
     res.status(200).json({
       total,
