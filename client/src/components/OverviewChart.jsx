3047e2324e4d5c3e245514f1b2b4e98784b5abe4
leliso <lelisoagegnehu491@gmail.com>
2023-05-09 22:26:42 +0300

    over view ui

---
+++ client/src/components/OverviewChart.jsx
@@ -0,0 +1,139 @@
+import { useTheme } from "@mui/material";
+import { useGetSalesQuery } from "../state/api";
+import { useMemo } from "react";
+import { ResponsiveLine } from "@nivo/line";
+
+// eslint-disable-next-line react/prop-types
+const OverviewChart = ({ isDashboard = false, view }) => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetSalesQuery();
+
+  const [totalSalesLine, totalUnitsLine] = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+
+    const { monthlyData } = data;
+    const totalSalesLine = {
+      id: "totalSales",
+      label: "Total Sales",
+      color: theme.palette.secondary.main,
+      data: [],
+    };
+    const totalUnitsLine = {
+      id: "totalUnits",
+      color: theme.palette.secondary[200],
+      data: [],
+    };
+
+    Object.values(monthlyData).reduce(
+      (acc, { month, totalSales, totalUnits }) => {
+        const curlSales = acc.sales + totalSales;
+        const curlUnits = acc.units + totalUnits;
+
+        totalSalesLine.data = [
+          ...totalSalesLine.data,
+          { x: month, y: curlSales },
+        ];
+        totalUnitsLine.data = [
+          ...totalUnitsLine.data,
+          { x: month, y: curlUnits },
+        ];
+
+        return {
+          sales: curlSales,
+          units: curlUnits,
+        };
+      },
+      { sales: 0, units: 0 }
+    );
+    return [[totalSalesLine], [totalUnitsLine]];
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  if (!data || isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <ResponsiveLine
+      data={view === "sales" ? totalSalesLine : totalUnitsLine}
+      margin={{ top: 20, right: 50, bottom: 50, left: 70 }}
+      xScale={{ type: "point" }}
+      yScale={{
+        type: "linear",
+        min: "auto",
+        max: "auto",
+        stacked: false,
+        reverse: false,
+      }}
+      yFormat=" >-.2f"
+      curve="catmullRom"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        format: (v) => {
+          if (isDashboard) {
+            return v.slice(0, 3);
+          }
+          return v;
+        },
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: isDashboard ? "" : "Month",
+        legendOffset: 36,
+        legendPosition: "middle",
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: isDashboard
+          ? ""
+          : `Total ${view === "sales" ? "Revenue" : "Units"} for Year`,
+        legendOffset: -40,
+        legendPosition: "middle",
+      }}
+      enableGridX={false}
+      enableGridY={false}
+      pointSize={10}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={
+        !isDashboard
+          ? [
+              {
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 30,
+                translateY: -40,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]
+          : undefined
+      }
+    />
+  );
+};
+
+export default OverviewChart;
---
+++ client/src/scenes/overview/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Header from "../../components/Header";
+import OverviewChart from "../../components/OverviewChart";
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+const Overview = () => {
+  const [view, setView] = useState("units");
+  return (
+    <Box>
+      <Header
+        title="OVERVIEW"
+        subtitle="Overview of general revenue and profit"
+      />
+      <Box height="75vh">
+        <FormControl sx={{ mt: "1rem" }}>
+          <InputLabel>View</InputLabel>
+          <Select
+            value={view}
+            label="view"
+            onChange={(e) => setView(e.target.value)}
+          >
+            <MenuItem value="sales">Sales</MenuItem>
+            <MenuItem value="units">Units</MenuItem>
+          </Select>
+        </FormControl>
+        <OverviewChart view={view} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Overview;
