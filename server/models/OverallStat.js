d2585c8bd1777f63c0801de47cb9d83134957fe9
leliso <lelisoagegnehu491@gmail.com>
2023-05-09 16:21:50 +0300

    overall stat model

---
+++ server/models/OverallStat.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const OverallStatSchema = new mongoose.Schema(
+  {
+    totalCustomer: Number,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: Number,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: {
+      date: String,
+      totalSales: Number,
+      totalUnits: Number,
+    },
+    salesByCategory: {
+      type: Map,
+      of: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const OverallStat = mongoose.model("OverallStat", OverallStatSchema);
+
+export default OverallStat;
