6061bfca9f289363e4d680755db6f4d5085eb393
leliso <lelisoagegnehu491@gmail.com>
2023-05-12 23:00:49 +0300

    break down route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import Geography from "./scenes/geography";
 import Overview from "./scenes/overview";
 import Daily from "./scenes/daily";
 import Monthly from "./scenes/monthly";
+import BreakDown from "./scenes/breakdown";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -33,6 +34,7 @@ function App() {
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<BreakDown />} />
             </Route>
           </Routes>
         </ThemeProvider>
