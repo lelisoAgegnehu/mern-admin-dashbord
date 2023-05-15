0c1196a90718e386630b9437ae8f5d87a0442e59
leliso <lelisoagegnehu491@gmail.com>
2023-05-15 16:42:33 +0300

    fix

--- server/routes/management.js
+++ server/routes/management.js
@@ -1,5 +1,5 @@
 import express from "express";
-import getAdmins from "../controllers/management.js";
+import { getAdmins } from "../controllers/management.js";
 
 const router = express.Router();
 router.get("/admins", getAdmins);
