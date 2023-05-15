5d06be741a670609ceb3f88a14f48238ecd52e34
leliso <lelisoagegnehu491@gmail.com>
2023-05-15 16:38:40 +0300

    admin route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import Overview from "./scenes/overview";
 import Daily from "./scenes/daily";
 import Monthly from "./scenes/monthly";
 import BreakDown from "./scenes/breakdown";
+import Admin from "./scenes/admin";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -35,6 +36,7 @@ function App() {
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
               <Route path="/breakdown" element={<BreakDown />} />
+              <Route path="/admin" element={<Admin />} />
             </Route>
           </Routes>
         </ThemeProvider>
