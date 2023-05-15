5e023bbaae8f2fde38279b0a01708a045cf3cea4
leliso <lelisoagegnehu491@gmail.com>
2023-05-15 16:32:24 +0300

    admin controller

---
+++ server/controllers/management.js
@@ -0,0 +1,10 @@
+import User from "../models/User";
+
+export const getAdmins = async (req, res) => {
+  try {
+    const admins = await User.find({ role: "admin" }).select("-password");
+    res.status(200).send(admins);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
