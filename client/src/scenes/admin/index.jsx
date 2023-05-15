b6c989a444f3086c388b42a4910faea62c82b8df
leliso <lelisoagegnehu491@gmail.com>
2023-05-15 16:49:55 +0300

    fetch admin data

--- client/src/scenes/admin/index.jsx
+++ client/src/scenes/admin/index.jsx
@@ -1,5 +1,19 @@
+import { Box } from "@mui/material";
+import { useGetAdminsQuery } from "../../state/api";
+import Header from "../../components/Header";
+
 const Admin = () => {
-  return <div>Admin</div>;
+  const { data, isLoading } = useGetAdminsQuery();
+
+  if (!data || isLoading) {
+    return <>Loading...</>;
+  }
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="ADMIN" subtitle="List of admin users" />
+    </Box>
+  );
 };
 
 export default Admin;
