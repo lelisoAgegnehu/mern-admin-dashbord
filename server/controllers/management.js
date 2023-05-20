92c7489e3453f03bbbcd5084027e0a279579dce7
leliso <lelisoagegnehu491@gmail.com>
2023-05-20 09:23:19 +0300

    user preformance  controller

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,9 +1,47 @@
+import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
 
 export const getAdmins = async (req, res) => {
   try {
     const admins = await User.find({ role: "admin" }).select("-password");
     res.status(200).send(admins);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
+
+export const getUserPerformance = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const userWithStats = await User.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(id) } },
+      {
+        $lookup: {
+          from: "affiliateStats",
+          localField: "_id",
+          foreignField: "userId",
+          as: "affiliateStats",
+        },
+      },
+      {
+        $unwind: "$affiliateStats",
+      },
+    ]);
+
+    const salesTransactions = await Promise.all(
+      userWithStats[0].affiliateStats.affiliateSales.map((id) => {
+        return Transaction.findById(id);
+      })
+    );
+
+    const filterSalesTransactions = salesTransactions.filter(
+      (transaction) => transaction !== null
+    );
+
+    res
+      .status(200)
+      .send({ user: userWithStats[0], sales: filterSalesTransactions });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
