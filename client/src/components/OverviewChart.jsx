f3f87c2fb8d2d61f60d926d09abb2c1f78f9d910
leliso <lelisoagegnehu491@gmail.com>
2023-06-01 23:08:37 +0300

    refactor overview cart

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -99,6 +99,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
       }}
       yFormat=" >-.2f"
       curve="catmullRom"
+      enableArea={isDashboard}
       axisTop={null}
       axisRight={null}
       axisBottom={{
@@ -116,6 +117,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
         legendPosition: "middle",
       }}
       axisLeft={{
+        tickValues: 5,
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
